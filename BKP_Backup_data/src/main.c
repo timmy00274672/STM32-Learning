e05c3027719cda864f93bb4dbdefbc2a866af422
timmy00274672 <timmy00274672@gmail.com>
2014-06-06 13:41:10 +0800

    Add information to BKP_Backup_data files

--- BKP_Backup_data/src/main.c
+++ BKP_Backup_data/src/main.c
@@ -1,3 +1,10 @@
+/*******************************************************************************
+* File Name         : main.c
+* Author            : timmy00274672 (timmy00274672@gmail.com)
+* Date              : 06/05/2014
+* Version           : Version 1.0
+* Description       : This source file for BKP_Backup_data
+*******************************************************************************/
 #include "main.h"
 #include "stm32f10x_lib.h"		
 #include "stm32f10x_pwr.h"
