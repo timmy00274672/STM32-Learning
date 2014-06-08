295bf27b88e9380470dc347abe89256f32ef262d
timmy00274672 <timmy00274672@gmail.com>
2014-06-08 16:54:26 +0800

    Add API to the head files

--- DeepSleep/inc/main.h
+++ DeepSleep/inc/main.h
@@ -7,10 +7,40 @@
 *******************************************************************************/
 #include "stm32f10x_lib.h"
 
+/**
+*	open clock on APB1 : PWR
+*	open clock on APB2 : GPIOA PA[0,4] used for I/O
+**/
 void RCC_Configuration(void);
+
+/**
+*	PA0 Input in IPU mode
+*	PA4 Output in PP mode @ 50 MHz
+**/
 void GPIO_Configuration(void);
+
+/**
+*	Set PA0 as EXTI source (Line 0) and enable it
+*	Mode : Interrupt
+*	Trigger : as falling 
+**/
 void EXTI_Configuration(void);
+
+/**
+*	setting priority grouping
+*	enable EXTI0_IRQHandler with proper priority
+**/
 void NVIC_Configuration(void);
+
+/**
+*	Select HCLK as SysTick clock souce(AHB clock selected as SysTick clock source.)
+*	Set Reload time as 100ms
+**/
 void SysTick_Configuration(void);
+
+/**
+*	Use SysTick counter to implement
+*	@param Tick time to delay in second
+**/
 void Delay_NSecond(u8 Tick);
 
--- DeepSleep/inc/stm32f10x_it.h
+++ DeepSleep/inc/stm32f10x_it.h
@@ -2,8 +2,11 @@
 * File Name      : 	stm32f10x_it.h
 * Author         : 	timmy00274672 (timmy00274672@gmail.com)
 * Date           : 	06/08/2014
-* Version        : 	Version 1.0
+* Version        : 	Version 1.1
 * Description    : 	This head file of overwritting handler for DeepSleep. 
 *******************************************************************************/
 
+/**
+*	Just clear the interrupt pending bit
+**/
 void EXTI0_IRQHandler(void);
